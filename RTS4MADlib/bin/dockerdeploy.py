8685f96f218d81851788786fcd1e127e3069ba43
spaladugu <spaladugu@pivotal.io>
2019-07-19 19:38:45 -0400

    change licence to apache 2.0

--- RTS4MADlib/bin/dockerdeploy.py
+++ RTS4MADlib/bin/dockerdeploy.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python
-#-----------------------------------------------------------------------------------------------
-# Copyright 2019 Sridhar Paladugu
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-# software and associated documentation files (the "Software"), to deal in the Software 
-# without restriction, including without limitation the rights to use, copy, modify, 
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
-# permit persons to whom the Software is furnished to do so, subject to the following 
-# conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies 
-# or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
-# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
-# OTHER DEALINGS IN THE SOFTWARE.
-#----------------------------------------------------------------------------------------------
 import subprocess
 import shlex
 import os
@@ -26,6 +7,24 @@ import logging
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
+"""
+    Name: kubernetesdeploy.py
+    <developers>
+        <developer>
+            <id>spaladugu</id>
+            <name>Sridhar Paladugu</name>
+            <email>spaladugu@pivotal.io</email>
+            <organization>Pivotal Software, Inc.</organization>
+            <organizationUrl>https://pivotal.io/pivotal-greenplum</organizationUrl>
+            <roles>
+                <role>Machine Learning Data Engineer </role>
+                <role>RTS-For-MADlib Lead/Committer</role>
+            </roles>
+        </developer>
+    </developers>
+    Description: A deployment tool for MADlib ML models on Docker Engine.
+
+"""
 
 class Docker:
 
